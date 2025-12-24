c2a3bafb027f0d7de848d929f859506741ef42a3
udsinarabadiutama-rgb <udsinarabadiutama@gmail.com>
2025-12-24 21:49:46 +0800

    Add target bank list in Bank.js
    Added a list of target banks for transactions.

---
+++ Bank.js
@@ -0,0 +1,12 @@
+const daftarBankTujuan = [
+  { "nama": "BANK CENTRAL ASIA (BCA)" },
+  { "nama": "BANK RAKYAT INDONESIA (BRI)" },
+  { "nama": "BANK MANDIRI" },
+  { "nama": "BANK NEGARA INDONESIA (BNI)" },
+  { "nama": "ALLO BANK" },
+  { "nama": "BANK SYARIAH INDONESIA (BSI)" },
+  { "nama": "BANK CIMB NIAGA" },
+  { "nama": "BANK PERMATA" },
+  { "nama": "BANK TABUNGAN NEGARA (BTN)" },
+  { "nama": "BANK DANAMON" }
+];
