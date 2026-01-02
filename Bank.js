35236af4f7046194cb7082bd64618709f1eb08f1
udsinarabadiutama-rgb <udsinarabadiutama@gmail.com>
2026-01-02 22:48:44 +0800

    Add logos to bank list in Bank.js

--- Bank.js
+++ Bank.js
@@ -1,12 +1,42 @@
-const daftarBankTujuan = [
-  { "nama": "BCA" },
-  { "nama": "BRI" },
-  { "nama": "BANK MANDIRI" },
-  { "nama": "BNI" },
-  { "nama": "ALLO BANK" },
-  { "nama": "BSI" },
-  { "nama": "BANK CIMB NIAGA" },
-  { "nama": "BANK PERMATA" },
-  { "nama": "BTN" },
-  { "nama": "BANK DANAMON" }
-];
+[
+  {
+    "nama": "BANK MEGA",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Bank_Mega_2013.svg/2560px-Bank_Mega_2013.svg.png"
+  },
+  {
+    "nama": "BANK MEGA SYARIAH",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/5/55/Logo_bank_mega_syariah_new.png"
+  },
+  {
+    "nama": "BCA",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/5/5c/Bank_Central_Asia.svg"
+  },
+  {
+    "nama": "BANK MANDIRI",
+    "logo": "https://upload.wikimedia.org/wikipedia/id/f/fa/Bank_Mandiri_logo.svg"
+  },
+  {
+    "nama": "BANK BRI",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/0/02/Bank_BRI_2000.svg"
+  },
+  {
+    "nama": "BANK BNI",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/Bank_Negara_Indonesia_logo_%282004%29.svg/2560px-Bank_Negara_Indonesia_logo_%282004%29.svg.png"
+  },
+  {
+    "nama": "BANK ALADIN SYARIAH",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/a/ad/Bank_Aladin.png"
+  },
+  {
+    "nama": "BANK CIMB NIAGA",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/3/38/CIMB_Niaga_logo.svg"
+  },
+  {
+    "nama": "BANK PERMATA",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Permata_Bank_%282024%29.svg/960px-Permata_Bank_%282024%29.svg.png"
+  },
+  {
+    "nama": "BANK PANIN",
+    "logo": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Logo_Panin_Bank.svg/1280px-Logo_Panin_Bank.svg.png"
+  }
+]
